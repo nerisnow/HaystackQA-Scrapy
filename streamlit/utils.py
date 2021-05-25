0459c75e5c9525253cfd19f693bde68f28c9f2fa
nerisnow <nirishamanandhar@gmail.com>
2021-05-25 16:19:05 +0545

    Added index mappings

--- streamlit/utils.py
+++ streamlit/utils.py
@@ -3,7 +3,7 @@ import streamlit as st
 import logging as logger
 #servicename:portnumber/predict
 # url = "https://565020e7c873.ngrok.io/predict"
-url = "https://f32edd2f96bd.ngrok.io/predict"
+url = "https://168de724f998.ngrok.io/predict"
 
 def format_request(question):
     """ 
