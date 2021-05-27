5f6890963568078944742f747e69661eced549fb
nerisnow <nirishamanandhar@gmail.com>
2021-05-27 15:01:47 +0545

    Removed extra reader

--- Elastic-DPR Haystack/app.py
+++ Elastic-DPR Haystack/app.py
@@ -41,7 +41,7 @@ p.add_node(component=es_retriever, name="ESRetriever1", inputs=["Query"])
 p.add_node(component=dpr_retriever, name="DPRRetriever1", inputs=["Query"])
 p.add_node(component=JoinDocuments(join_mode="concatenate"), name="JoinResults", inputs=["ESRetriever1", "DPRRetriever1"])
 p.add_node(component=reader, name="Reader", inputs=["JoinResults"])
-p.add_node(component=reader, name="Reader", inputs=["DPRRetriever1"])
+# p.add_node(component=reader, name="Reader", inputs=["DPRRetriever1"])
 
 @app.route('/', methods=["GET"])
 def health():
