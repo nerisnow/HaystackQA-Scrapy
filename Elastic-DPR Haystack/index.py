35f15b3d7fe8b9f1ca513a3eaf143acb44a2bcb4
nerisnow <nirishamanandhar@gmail.com>
2021-06-02 19:45:12 +0545

    Removed edited code

--- Elastic-DPR Haystack/index.py
+++ Elastic-DPR Haystack/index.py
@@ -14,14 +14,6 @@ logger.basicConfig(level="INFO")
 #path for the .txt data files
 local_doc_dir = "/content/HaystackQA-Scrapy/amitness/data"
 
-# instance for document store
-document_store = ElasticsearchDocumentStore(
-    host="localhost", username="", password="", index="amitblogs")
-
-# clearing any previously indexed documents
-document_store.delete_all_documents()
-
-
 ## download and prepare scraped data 
 # s3 instance api
 s3_url = ""
@@ -29,15 +21,18 @@ s3_url = ""
 # directory to store fetched s3 data
 output_dir = ""
 
-# fetch_archive_from_http(url=s3_url, output_dir=output_dir)
 
-# Convert files to dicts
-# dicts = convert_files_to_dicts(
-#     dir_path=output_dir,
-#     clean_func=clean_wiki_text,
-#     split_paragraphs=True)
+# instance for document store
+document_store = ElasticsearchDocumentStore(
+    host="localhost",username = "",password="",index="amitblogs")
+logger.info("Connected to ES")
+
+
+# clearing any previously indexed documents
+# document_store.delete_all_documents()
 
 filename  = "/content/HaystackQA-Scrapy/amitness/blogs.jl"
+filename = "/home/nirisha/amitness/blogs.jl"
 list_of_dict = []
 
 with open(filename, 'rb') as f:
@@ -56,7 +51,7 @@ retriever = DensePassageRetriever(document_store=document_store,
                                   max_seq_len_query=64,
                                   max_seq_len_passage=256,
                                   batch_size=16,
-                                  use_gpu=True,
+                                  use_gpu=False,
                                   embed_title=True,
                                   use_fast_tokenizers=True)
 logger.info("updating embedding.")
